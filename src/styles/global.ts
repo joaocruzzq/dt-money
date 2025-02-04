42d28ba220997f2c15308887c8edf5e0da3ae429
joão pedro cruz <joaocruzzdev@gmail.com>
2025-02-04 14:28:58 -0300

    configuraçoes iniciais e definindo cores

---
+++ src/styles/global.ts
@@ -0,0 +1,26 @@
+import { createGlobalStyle } from "styled-components";
+
+export const  GlobalStyle = createGlobalStyle`
+   * {
+      margin: 0;
+      padding: 0;
+
+      box-sizing: border-box;
+   }
+
+   :focus {
+      outline: 0;
+      box-shadow: 0 0 0 2px ${props => props.theme["green-500"]};
+   }
+
+   body {
+      -webkit-font-smoothing: antialiased;
+      
+      color: ${props => props.theme["gray-100"]};
+      background: ${props => props.theme["gray-800"]};
+   }
+
+   body, input, text-area, button {
+      font: 400 1rem Roboto, sans-serif;
+   }
+`
