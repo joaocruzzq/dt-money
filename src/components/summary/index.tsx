c889a28b76d5ffc2396e187b2cb9d033ff32a56b
joão pedro cruz <joaocruzzdev@gmail.com>
2025-02-06 10:12:07 -0300

    utilizando hooks personalizados

--- src/components/summary/index.tsx
+++ src/components/summary/index.tsx
@@ -2,29 +2,12 @@ import { SummaryCard, SummaryContainer } from "./styles";
 
 import { ArrowCircleDown, ArrowCircleUp, CurrencyDollar } from "phosphor-react";
 
-import { useContext } from "react";
-import { TransactionsContext } from "../../contexts/transactions-context";
 import { priceFormatter } from "../../utils/formatter";
 
+import { useSummery } from "../../hooks/useSummary";
+
 export function Summary() {
-   const { transactions } = useContext(TransactionsContext)
-
-   const summary = transactions.reduce(
-      (acc, transaction) => {
-         if (transaction.type === "income") {
-            acc.income += transaction.price
-            acc.total += transaction.price
-         }
-
-         else {
-            acc.outcome += transaction.price
-            acc.total -= transaction.price
-         }
-
-         return acc
-      },
-      
-      { income: 0, outcome: 0, total: 0 })
+   const summary = useSummery()
 
    return (
       <SummaryContainer>
