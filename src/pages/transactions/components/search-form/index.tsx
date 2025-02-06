ca0ae36fc46e94d63ec445ac6cdd0eb64d2c02d8
joão pedro cruz <joaocruzzdev@gmail.com>
2025-02-06 12:46:46 -0300

    incrementando busca no componente search form

--- src/pages/transactions/components/search-form/index.tsx
+++ src/pages/transactions/components/search-form/index.tsx
@@ -6,6 +6,9 @@ import { MagnifyingGlass } from "phosphor-react";
 import * as z from "zod"
 import { zodResolver } from "@hookform/resolvers/zod"
 
+import { useContext } from "react";
+import { TransactionsContext } from "../../../../contexts/transactions-context";
+
 const SearchFormSchema = z.object({
    query: z.string()
 })
@@ -13,14 +16,14 @@ const SearchFormSchema = z.object({
 type SearchFormInputs = z.infer<typeof SearchFormSchema>
 
 export function SearchForm() {
+   const { fetchTransactions } = useContext(TransactionsContext)
+
    const { register, handleSubmit, formState: {isSubmitting} } = useForm<SearchFormInputs>({
       resolver: zodResolver(SearchFormSchema)
    })
 
    async function handleSearchTransactions(data: SearchFormInputs) {
-      await new Promise(resolve => setTimeout(resolve, 2000))
-
-      console.log(data)
+      await fetchTransactions(data.query)
    }
 
    return (
